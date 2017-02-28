73b061ecb85f4f06ae3d156a5899d9a85f10aff2
mario <mario@mfjassociates.net>
2017-02-27 21:03:43 -0500

    Remove unused import

--- src/main/java/net/mfjassociates/tools/ClasspathHelper.java
+++ src/main/java/net/mfjassociates/tools/ClasspathHelper.java
@@ -2,7 +2,6 @@ package net.mfjassociates.tools;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
