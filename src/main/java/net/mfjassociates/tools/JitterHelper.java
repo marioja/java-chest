5418563732a056260a0e47b8f7e7b482c0389d81
mario <mario@mfjassociates.net>
2017-11-04 00:05:18 -0400

    Added JitterHelper to test how much jitter is found on a system

---
+++ src/main/java/net/mfjassociates/tools/JitterHelper.java
@@ -0,0 +1,56 @@
+package net.mfjassociates.tools;
+
+import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class JitterHelper {
+
+	private static ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+	private static CompletableFuture<Long> cf = new CompletableFuture<Long>();
+	private static Callable<Long> task = () -> {
+		return System.nanoTime();
+	};
+	private static Runnable task2 = () -> {
+		long l=System.nanoTime();
+		cf.complete(l);
+	};
+	private static final int DELAY = 3;
+	private static final long DELAY_NANO = DELAY * 1000000000l;
+	private static final DescriptiveStatistics stats=new DescriptiveStatistics();
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		Runtime.getRuntime().addShutdownHook(new Thread( () -> {System.out.println(MessageFormat.format("Stats: mean={1}, variance={2}, min={3}, max={4}", 0, stats.getMean(), stats.getVariance(), stats.getMin(), stats.getMax()));}));
+		executor.scheduleAtFixedRate(task2, 3, 3, TimeUnit.SECONDS);
+		long p=System.nanoTime();
+		long c;
+		long diff;
+		while (true) {
+			c=cf.get();
+			diff=DELAY_NANO - (c-p);
+			stats.addValue(diff);
+			cf=new CompletableFuture<Long>();
+			System.out.println(MessageFormat.format("Difference between ticks={0}, mean={1}, variance={2}, min={3}, max={4}", diff, stats.getMean(), stats.getVariance(), stats.getMin(), stats.getMax()));
+			p=c;
+		}
+	}
+	private static final String[] UNITS = new String[] {"nano","micro","milli","second"};
+	private static String convert(long diff) {
+		float lasti=1;
+		int unitI=-1;
+		for (long i = 1l; i < Math.abs(diff); i=i*1000) {
+			lasti=i;
+			unitI++;
+		}
+		if (unitI==-1) unitI=0; // if diff was 0, make sure it is in nano
+		Float fdiff=diff/lasti;
+		return fdiff.toString()+" "+UNITS[unitI];
+	}
+
+	
+}
